d2edd2be8f740ed2446f15037ffc9a8163e5f9e6
tlackiesapidyne <tlackie@sapidyne.com>
2024-07-31 13:21:13 -0600

    Increased Z homing speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2218,7 +2218,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (50*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
