6158e704617907dadba66d4aa083cd3cd820e2db
tlackiesapidyne <tlackie@sapidyne.com>
2024-08-27 10:11:41 -0600

    Z_MAX to 600 and X_BED to 600

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1750,7 +1750,7 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 200
+#define X_BED_SIZE 600
 #define Y_BED_SIZE 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
@@ -1759,7 +1759,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 400
+#define Z_MAX_POS 600
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
